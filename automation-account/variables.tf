ca40e2554a9ee78d9aaf9a895dea63186c751639
Brijen Shah <brijen.shah@live.com>
2021-06-27 21:52:52 -0700

    Update variables.tf

--- automation-account/variables.tf
+++ automation-account/variables.tf
@@ -5,3 +5,7 @@ variable "prefix" {
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
 }
+
+variable "rgname" {
+  description = "The Azure Resource Group Name to deploy automation account."
+}
